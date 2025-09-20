e656727189b999dadaeee4c0add43892975f463a
Pi-Eatery <170902488+Pi-Eatery@users.noreply.github.com>
2025-09-20 02:41:18 +0000

    Added some logic to main.py to flesh it out

--- pipelinedquotes/main.py
+++ pipelinedquotes/main.py
@@ -1,4 +1,13 @@
-import requests
 from fastapi import FastAPI
-def health_ping():
-    api_enpoint = "/health"
+
+def __init__(self, endpoint):
+    self.endpoint = "/health"
+
+def root():
+    return {{"status": "ok"}, "200 OK"}
+
+def health_ping(self):
+    app = FastAPI()
+    @app.get(self.endpoint)
+    def root():
+        return {{"status": "ok"}, "200 OK"}
